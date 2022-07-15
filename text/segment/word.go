0235b9202c44aef6f81dff8b20c4ac0b452f538a
Will Daly <will.e.daly@gmail.com>
2022-07-15 07:14:02 -0700

    wip

--- text/segment/word.go
+++ text/segment/word.go
@@ -5,57 +5,75 @@ package segment
 // Information symbol (U+2139) is both ALetter and Extended_Pictographic, so we need a separate table for Extended_Pictographic.
 //go:generate go run gen_props.go --prefix wbe --dataPath data/emoji-data.txt --propertyName Extended_Pictographic --outputPath word_emoji_props.go
 
-/*
-Break at the start and end of text, unless the text is empty.
-WB1 	sot 	÷ 	Any
-WB2 	Any 	÷ 	eot
+// WordBreaker finds breakpoints between words.
+// This complies with https://www.unicode.org/reports/tr29/#Word_Boundaries
+type WordBreaker struct {
+	lastProp wbProp
+}
 
-Do not break within CRLF.
-WB3 	CR 	× 	LF
+func (wb *WordBreaker) ProcessRune(r rune) (canBreakBefore bool) {
+	prop := wbPropForRune(r)
 
-Otherwise break before and after Newlines (including CR and LF)
-WB3a 	(Newline | CR | LF) 	÷
-WB3b 	  	÷ 	(Newline | CR | LF)
+	// WB1 sot ÷ Any
+	// WB2 Any ÷ eot
+	// We don't need to implement these because we're only interested in non-empty segments.
 
-Do not break within emoji zwj sequences.
-WB3c 	ZWJ 	× 	\p{Extended_Pictographic}
+	// WB3 CR × LF
+	if prop == wbPropLF && wb.lastProp == wbPropCR {
+		canBreakBefore = false
+		goto done
+	}
 
-Keep horizontal whitespace together.
-WB3d 	WSegSpace 	× 	WSegSpace
+	/*
+		Otherwise break before and after Newlines (including CR and LF)
+		WB3a (Newline | CR | LF) ÷
+		WB3b   ÷ (Newline | CR | LF)
 
-Ignore Format and Extend characters, except after sot, CR, LF, and Newline. (See Section 6.2, Replacing Ignore Rules.) This also has the effect of: Any × (Format | Extend | ZWJ)
-WB4 	X (Extend | Format | ZWJ)* 	→ 	X
+		Do not break within emoji zwj sequences.
+		WB3c ZWJ × \p{Extended_Pictographic}
 
-Do not break between most letters.
-WB5 	AHLetter 	× 	AHLetter
+		Keep horizontal whitespace together.
+		WB3d WSegSpace × WSegSpace
 
-Do not break letters across certain punctuation.
-WB6 	AHLetter 	× 	(MidLetter | MidNumLetQ) AHLetter
-WB7 	AHLetter (MidLetter | MidNumLetQ) 	× 	AHLetter
-WB7a 	Hebrew_Letter 	× 	Single_Quote
-WB7b 	Hebrew_Letter 	× 	Double_Quote Hebrew_Letter
-WB7c 	Hebrew_Letter Double_Quote 	× 	Hebrew_Letter
+		Ignore Format and Extend characters, except after sot, CR, LF, and Newline. (See Section 6.2, Replacing Ignore Rules.) This also has the effect of: Any × (Format | Extend | ZWJ)
+		WB4 X (Extend | Format | ZWJ)* → X
 
-Do not break within sequences of digits, or digits adjacent to letters (“3a”, or “A3”).
-WB8 	Numeric 	× 	Numeric
-WB9 	AHLetter 	× 	Numeric
-WB10 	Numeric 	× 	AHLetter
+		Do not break between most letters.
+		WB5 AHLetter × AHLetter
 
-Do not break within sequences, such as “3.2” or “3,456.789”.
-WB11 	Numeric (MidNum | MidNumLetQ) 	× 	Numeric
-WB12 	Numeric 	× 	(MidNum | MidNumLetQ) Numeric
+		Do not break letters across certain punctuation.
+		WB6 AHLetter × (MidLetter | MidNumLetQ) AHLetter
+		WB7 AHLetter (MidLetter | MidNumLetQ) × AHLetter
+		WB7a Hebrew_Letter × Single_Quote
+		WB7b Hebrew_Letter × Double_Quote Hebrew_Letter
+		WB7c Hebrew_Letter Double_Quote × Hebrew_Letter
 
-Do not break between Katakana.
-WB13 	Katakana 	× 	Katakana
+		Do not break within sequences of digits, or digits adjacent to letters (“3a”, or “A3”).
+		WB8 Numeric × Numeric
+		WB9 AHLetter × Numeric
+		WB10 Numeric × AHLetter
 
-Do not break from extenders.
-WB13a 	(AHLetter | Numeric | Katakana | ExtendNumLet) 	× 	ExtendNumLet
-WB13b 	ExtendNumLet 	× 	(AHLetter | Numeric | Katakana)
+		Do not break within sequences, such as “3.2” or “3,456.789”.
+		WB11 Numeric (MidNum | MidNumLetQ) × Numeric
+		WB12 Numeric × (MidNum | MidNumLetQ) Numeric
 
-Do not break within emoji flag sequences. That is, do not break between regional indicator (RI) symbols if there is an odd number of RI characters before the break point.
-WB15 	sot (RI RI)* RI 	× 	RI
-WB16 	[^RI] (RI RI)* RI 	× 	RI
+		Do not break between Katakana.
+		WB13 Katakana × Katakana
 
-Otherwise, break everywhere (including around ideographs).
-WB999 	Any 	÷ 	Any
-*/
+		Do not break from extenders.
+		WB13a (AHLetter | Numeric | Katakana | ExtendNumLet) × ExtendNumLet
+		WB13b ExtendNumLet × (AHLetter | Numeric | Katakana)
+
+		Do not break within emoji flag sequences. That is, do not break between regional indicator (RI) symbols if there is an odd number of RI characters before the break point.
+		WB15 sot (RI RI)* RI × RI
+		WB16 [^RI] (RI RI)* RI × RI
+
+		Otherwise, break everywhere (including around ideographs).
+		WB999 Any ÷ Any
+	*/
+
+done:
+	wb.lastProp = prop
+
+	return canBreakBefore
+}
